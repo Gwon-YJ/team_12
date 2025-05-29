19df186b71d92998fd3f88fdd8886c28a43d7924
San Kang <>
2025-05-29 16:07:33 +0900

    엔티티수정

--- src/main/java/org/example/newsfeed/entity/Follow.java
+++ src/main/java/org/example/newsfeed/entity/Follow.java
@@ -2,45 +2,32 @@ package org.example.newsfeed.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "follow")
-public class Follow extends BaseEntity{
+public class Follow extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long followId;
+    private Long id;
 
+    // 팔로우 거는 사람
+    @ManyToOne
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
 
-    @Column(nullable = false)
-    private Long follows;
+    // 팔로우 대상
+    @ManyToOne
+    @JoinColumn(name = "following_id", nullable = false)
+    private User following;
 
-    public Follow(){}
-
-    public Follow(Long follows){
-        this.follows =follows;
+    public Follow(User follower, User following) {
+        this.follower = follower;
+        this.following = following;
     }
-
-    @ManyToOne
-    @JoinColumn(name = "like_id")
-    @Setter
-    private Likes like;
-
-    @ManyToOne
-    @JoinColumn(name = "comment_id")
-    @Setter
-    private Comment comment;
-
-    @ManyToOne
-    @JoinColumn(name = "post_id")
-    @Setter
-    private Post post;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    @Setter
-    private User user;
-
 }
