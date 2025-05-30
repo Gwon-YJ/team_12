19aced4e606f9c88636190bef68fcb3283ebcae2
Ddiy0ng <bueong0627@gmail.com>
2025-05-30 15:26:28 +0900

    Chore: GlobalExceptionHandler의 불필요한 주석 삭제

--- src/main/java/com/example/newsfeed/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/newsfeed/exception/GlobalExceptionHandler.java
@@ -11,30 +11,4 @@ public class GlobalExceptionHandler {
         ErrorType errorType = customeException.getErrorType();
         return new ResponseEntity<>(errorType.getErrorMessage(), errorType.getHttpStatus());
     }
-
-    /*
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> entityNotFoundHandler(){
-        return new ResponseEntity<>(ErrorType.ENTITY_NOT_FOUND.getErrorMessage(), ErrorType.ENTITY_NOT_FOUND.getHttpStatus());
-    }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> methodArgumentNotValid(){
-        return new ResponseEntity<>(ErrorType.METHOD_ARGUMENT_NOT_VALID.getErrorMessage(), ErrorType.METHOD_ARGUMENT_NOT_VALID.getHttpStatus());
-    }
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<String> accessDenied(){
-        return new ResponseEntity<>(ErrorType.ACCESS_DENIED.getErrorMessage(), ErrorType.ACCESS_DENIED.getHttpStatus());
-    }
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<String> authentication(){
-        return new ResponseEntity<>(ErrorType.LOGIN_AUTHENTICATION.getErrorMessage(), ErrorType.LOGIN_AUTHENTICATION.getHttpStatus());
-    }
-    @ExceptionHandler(PassworMismatchException.class)
-    public ResponseEntity<String> passwordMismatch(){
-        return new ResponseEntity<>(ErrorType.PASSWORD_MISMATCH.getErrorMessage(), ErrorType.PASSWORD_MISMATCH.getHttpStatus());
-    }
-
-     */
-
-
 }
